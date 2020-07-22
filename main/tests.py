ee4f1ffab24ad67a4d1d0262b109d17b1688d1b1
Richard Walker <rwalker@redhat.com>
2020-07-22 15:47:32 +0100

    added unit tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class UnitTestCase(TestCase):
+
+    def test_case_connection(self):
+        response = self.client.get('/deck/')
+        assert response.status_code == 200
+
+    def test_case_json(self):
+        response = self.client.get('/deck/')
+        self.assertContains(response, "ace")
+        self.assertContains(response, "spades")
